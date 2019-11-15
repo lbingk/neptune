7b56ad9519019c65dd13fad526cb0acf89978bcb
luobingkai <lbk@imlaidian.com>
2019-11-15 18:57:14 +0800

    提交遗漏的序列化代码

--- neptune-serializable/src/main/java/org/net/api/jdk/JdkSerialization.java
+++ neptune-serializable/src/main/java/org/net/api/jdk/JdkSerialization.java
@@ -1,9 +1,11 @@
 package org.net.api.jdk;
 
 import io.netty.buffer.ByteBuf;
+import org.msgpack.MessagePack;
+import org.msgpack.type.Value;
 import org.net.api.SerializationUtil;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -16,26 +18,20 @@ public class JdkSerialization implements SerializationUtil {
 
     @Override
     public byte[] serialize(Object o, ByteBuf byteBuf) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(o);
-//        oos.flush();
-        byte[] bytes = bos.toByteArray();
-//        byteBuf.writeBytes(bytes);
-        bos.close();
-        oos.close();
-        return bytes;
+        MessagePack messagePack = new MessagePack();
+        /** 序列化对象*/
+        byte[] raw = messagePack.write(o);
+        byteBuf.writeBytes(raw);
+        return raw;
     }
 
     @Override
-    public void deserialize(ByteBuf byteBuf, List<Object> list) throws Exception {
-        final int length = byteBuf.readableBytes();
-        final byte[] b = new byte[length];
-        byteBuf.getBytes(byteBuf.readerIndex(), b, 0, length);
-
-        ByteArrayInputStream bis = new ByteArrayInputStream(b);
-        ObjectInputStream ois = new ObjectInputStream(bis);
-        list.add(ois.readObject());
-        ois.close();
+    public void deserialize(ByteBuf byteBuf, List<Object> list) throws IOException {
+        int length = byteBuf.readableBytes();
+        byte[] array = new byte[length];
+        byteBuf.getBytes(byteBuf.readerIndex(), array, 0, length);
+        MessagePack messagePack = new MessagePack();
+        Value read = messagePack.read(array);
+        list.add(read);
     }
 }
